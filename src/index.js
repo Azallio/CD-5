dbdb2eb44526f4e5d231a8a25f363494a1157596
Azallio <allio.all.in.one@gmail.com>
2025-05-24 16:28:56 +0300

    1.0.1

--- src/index.js
+++ src/index.js
@@ -1,44 +1,45 @@
-const titleOfBank = document.querySelector("#titleOfBank")
-const typeOfPay = document.querySelector("#typeOfPay")
-const cardNumber = document.querySelector("#cardNumber")
-const ownerName = document.querySelector("#ownerName")
-const endDataOfCard = document.querySelector("#endDataOfCard")
-const Card = document.querySelector("#Card")
+const titleOfBank = document.querySelector("#titleOfBank");
+const typeOfPay = document.querySelector("#typeOfPay");
+const cardNumber = document.querySelector("#cardNumber");
+const ownerName = document.querySelector("#ownerName");
+const endDataOfCard = document.querySelector("#endDataOfCard");
+const Card = document.querySelector("#Card");
 
 
 
 const updateCardPreview = () => {
-    let spans = Card.children
-    spans[0].innerHTML = titleOfBank.value
-    spans[1].innerHTML = cardNumber.value
-    let spansInDiv = spans[2].children
-    spansInDiv[0].innerHTML = ownerName.value
-    spansInDiv[1].innerHTML = endDataOfCard.value
-    spansInDiv[2].innerHTML = typeOfPay.value
-}
+    let spans = Card.children;
+    spans[0].innerHTML = titleOfBank.value;
+    spans[1].innerHTML = cardNumber.value;
+    let spansInDiv = spans[2].children;
+    spansInDiv[0].innerHTML = ownerName.value;
+    spansInDiv[1].innerHTML = endDataOfCard.value;
+    spansInDiv[2].innerHTML = typeOfPay.value;
+};
 
 const createCell = (text) => {
-    const td = document.createElement("td")
-    td.className = "border px-4 py-2 text-center"
-    td.innerText = text
-    return td
-}
+    const td = document.createElement("td");
+    td.className = "border px-4 py-2 text-center";
+    td.innerText = text;
+    return td;
+};
 
 const savedCardInfoInTable = () => {
-    const table = document.querySelector("table")
-    const tr = document.createElement("tr")
+    const table = document.querySelector("table");
+    table.className = "w-full table-auto border-collapse text-white text-2xl";
+    const tr = document.createElement("tr");
     tr.append(
         createCell(titleOfBank.value),
         createCell(typeOfPay.value),
         createCell(cardNumber.value),
         createCell(ownerName.value),
         createCell(endDataOfCard.value)
-    )
-    table.append(tr)
-}
+    );
+    table.append(tr);
+};
 
-titleOfBank.addEventListener('input', updateCardPreview)
-typeOfPay.addEventListener('change', updateCardPreview)
-cardNumber.addEventListener('input', updateCardPreview)
-ownerName.addEventListener('input', updateCardPreview)
-endDataOfCard.addEventListener('change', updateCardPreview)
+titleOfBank.addEventListener('input', updateCardPreview);
+typeOfPay.addEventListener('change', updateCardPreview);
+cardNumber.addEventListener('input', updateCardPreview);
+ownerName.addEventListener('input', updateCardPreview);
+endDataOfCard.addEventListener('change', updateCardPreview);
